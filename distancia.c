3b51c75487cfa3b100440ec008297499cb811769
palaec00 <palaec00@estudiantes.unileon.es>
2019-03-27 17:10:30 +0100

    ejercicio 1 enunciado

---
+++ distancia.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+void cargar(int v[2]);
+float distancia(int v1[2],int v2[2]);
+int main ()
+  {
+  int result;
+  int v1[2];
+  int v2[2];
+  cargar (v1);
+  cargar (v2);
+  distancia(v1,v2);
+  }
+void cargar(int v[2]){
+   int i;
+    for(i=0;i<2;i++){
+       scanf("%d",&v[i]);
+}
+}
+float distancia(int v1[2],int v2[2]){
+    int resta1;
+    int resta2;
+       resta1=v2[0]-v1[0];
+       resta2=v2[1]-v1[1];
+    int result;
+       result=sqrt(pow(resta1,2)+pow(resta2,2));
+       return result;
+}
